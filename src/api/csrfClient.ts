74a5894158206df51e6a8693695449fcf23e5fb9
Alif_04 <alifnaufalh@gmail.com>
2025-07-21 06:41:07 +0700

    hotfix: integrate env to apps

--- src/api/csrfClient.ts
+++ src/api/csrfClient.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const csrfClient = axios.create({
-  baseURL: "http://localhost:8000",  
+  baseURL: import.meta.env.VITE_API_URL,  
   withCredentials: true,
   withXSRFToken: true,
 });
